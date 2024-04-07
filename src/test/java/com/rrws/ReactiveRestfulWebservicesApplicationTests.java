0f65d78ba6392ab85d400badb480b53377d5d65f
Vishall0317 <vishalbramhankar18@gmail.com>
2024-04-07 21:22:56 +0530

    initial commit

---
+++ src/test/java/com/rrws/ReactiveRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
